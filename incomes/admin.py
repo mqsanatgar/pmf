3c091cefca840bd7243a3e1fc27f1901c8f0c7d8
mqsanatgar <masoud.sanatgar.quchani@gmail.com>
2023-11-15 10:56:35 +0330

    Income models also added to the project and admin panel

--- incomes/admin.py
+++ incomes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Incomes
 
 # Register your models here.
+
+admin.site.register(Incomes)
--- incomes/models.py
+++ incomes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Incomes(models.Model):
+
+    """Docstring for Incomes."""
+
+    name = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
